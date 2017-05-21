3f0eb697040ea8991bbeb67452a21ee9d90a7666
Your Name <Your email>
2017-05-21 12:35:58 +1000

    hobbit_body_parts

---
+++ src/clojure_noob/hobbit_body_parts.clj
@@ -0,0 +1,47 @@
+(ns clojure-noob.core
+  (:gen-class))
+
+(def assym-hobbit-body-parts [{:name "head" :size 3}
+                              {:name "lef-eye" :size 1}
+                              {:name "left-ear" :size 1}
+                              {:name "mouth" :size 1}
+                              {:name "nose" :size 1}
+                              {:name "neck" :size 2}
+                              {:name "left-shoulder" :size 3}
+                              {:name "left-upper-arm" :size 3}
+                              {:name "chest" :size 10}
+                              {:name "back" :size 10}
+                              {:name "left-forearm" :size 3}
+                              {:name "abdomen" :size 6}
+                              {:name "left-kidney" :size 1}
+                              {:name "left-hand" :size 2}
+                              {:name "left-knee" :size 2}
+                              {:name "left-thigh" :size 4}
+                              {:name "left-lower-leg" :size 3}
+                              {:name "left-achilles" :size 1}
+                              {:name "left-foot" :size 2}])
+
+
+(defn replace-left-right [part]
+  (def replaced-name (clojure.string/replace (:name part) #"^left-" "right-" ))
+  {:name replaced-name :size (:size part)})
+
+(replace-left-right {:name "left-data" :size 1})  
+
+
+(defn symmetrize-body-part 
+"Expects a sequence of maps of body parts with :name and :size"
+[asym-body-parts]
+  (loop [remaining-body-parts asym-body-parts 
+         finalbodyparts []]
+   (if (empty? remaining-body-parts)
+      finalbodyparts
+      (let [[first-body-part & rem-body-parts] remaining-body-parts]
+           (recur rem-body-parts 
+                (into finalbodyparts 
+                     (set [first-body-part 
+                          (replace-left-right first-body-part)])))))))
+
+;; uncomment below code and run
+(println (clojure.string/join "\n " (symmetrize-body-part assym-hobbit-body-parts)))
+
